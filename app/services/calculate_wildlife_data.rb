e281f9a8f1da7bc3addee544f8c8a2762a9eed03
Kelly Munro <kelly.munro@fluxfederation.com>
2019-06-13 09:59:08 +1200

    Calculate the most frequent isos, month and year for wildlife images

--- app/services/calculate_wildlife_data.rb
+++ app/services/calculate_wildlife_data.rb
@@ -4,6 +4,9 @@ class CalculateWildlifeData
   def call
     {
       focal_lengths: most_frequent_focal_lengths,
+      isos: most_frequent_isos,
+      month: most_frequent_month,
+      year: most_frequent_year
     }
   end
 
@@ -15,7 +18,31 @@ class CalculateWildlifeData
     results.map { |focal_length| focal_length.round }
   end
 
+  def most_frequent_isos
+    isos = Exif.wildlife.pluck(:isoSpeedRating)
+    results = calculate_frequencies(isos)
+    results.map { |iso| iso.round }
+  end
+
+  def most_frequent_month
+    months = Exif.wildlife.pluck(:dateMonth)
+    month = calculate_most_frequent(months)&.to_i
+
+    if month.present?
+      Date::MONTHNAMES[month]
+    end
+  end
+
+  def most_frequent_year
+    years = Exif.wildlife.pluck(:dateYear)
+    calculate_most_frequent(years)&.round
+  end
+
   def calculate_frequencies(data)
     FrequencyCalculator.calculate_frequently_used(data, 5)
+  end
+
+  def calculate_most_frequent(data)
+    FrequencyCalculator.calculate_most_frequent(data)
   end
 end
--- spec/services/calculate_wildlife_data_spec.rb
+++ spec/services/calculate_wildlife_data_spec.rb
@@ -19,5 +19,35 @@ RSpec.describe CalculateWildlifeData, type: :service do
         expect(service.call[:focal_lengths]).to eq [400, 350]
       end
     end
+
+    context 'isos' do
+      before do
+        exif_1.update!(isoSpeedRating: 100.0)
+      end
+
+      it 'returns the most frequently used isos for wildlife' do
+        expect(service.call[:isos]).to eq [1600, 100]
+      end
+    end
+
+    context 'month' do
+      before do
+        exif_2.update!(dateMonth: 11.0)
+      end
+
+      it 'returns the most frequent month for wildlife images' do
+        expect(service.call[:month]).to eq 'February'
+      end
+    end
+
+    context 'year' do
+      before do
+        exif_2.update!(dateYear: 2017.0)
+      end
+
+      it 'returns the most frequent year for wildlife images' do
+        expect(service.call[:year]).to eq 2019
+      end
+    end
   end
 end
