dba87973bb27db1e844200ce9f66cff924478d19
Kelly Munro <kelly.munro@fluxfederation.com>
2019-07-24 16:41:04 +1200

    Add additional data to statistics by lens

--- app/services/calculate_data_by_lens.rb
+++ app/services/calculate_data_by_lens.rb
@@ -10,7 +10,14 @@ class CalculateDataByLens
   def call
     {
       lens: lens_name,
-      keywords: keywords_by_lens
+      keywords: keywords_by_lens,
+      cameras: cameras_by_lens,
+      focal_lengths: focal_lengths_by_lens,
+      shutter_speeds: shutter_speeds_by_lens,
+      isos: isos_by_lens,
+      months: months_by_lens,
+      years: years_by_lens,
+      month_year_combinations: month_year_combinations_by_lens
     }
   end
 
@@ -24,7 +31,58 @@ class CalculateDataByLens
     frequencies.flat_map do |frequency|
       Keyword.where(id_local: frequency).pluck(:lc_name)
     end
+  end
 
+  def cameras_by_lens
+    camera_ids = Exif.by_lens(lens_ids).pluck(:cameraModelRef).compact
+    FrequencyCalculator.calculate_frequently_used_from_model(camera_ids, Camera, 5)
+  end
+
+  def focal_lengths_by_lens
+    focal_lengths = Exif.by_lens(lens_ids).pluck(:focalLength)
+
+    results = calculate_frequently_used(focal_lengths)
+    results.map { |result| result.round }
+  end
+
+  def shutter_speeds_by_lens
+    shutter_speeds = Exif.by_lens(lens_ids).map do |exif|
+      exif.shutter_speed_value
+    end.compact
+
+    calculate_frequently_used(shutter_speeds)
+  end
+
+  def isos_by_lens
+    isos = Exif.by_lens(lens_ids).pluck(:isoSpeedRating)
+    results = calculate_frequently_used(isos)
+    results.map { |result| result.round }
+  end
+
+  def months_by_lens
+    months = Exif.by_lens(lens_ids).pluck(:dateMonth)
+
+    results = calculate_frequently_used(months)
+    results.map do |result|
+      Date::MONTHNAMES[result]
+    end
+  end
+
+  def years_by_lens
+    years = Exif.by_lens(lens_ids).pluck(:dateYear)
+
+    results = calculate_frequently_used(years)
+    results.map do |result|
+      result.round
+    end
+  end
+
+  def month_year_combinations_by_lens
+    month_years = Exif.by_lens(lens_ids).map do |exif|
+      exif.month_and_year
+    end.compact
+
+    calculate_frequently_used(month_years)
   end
 
   def calculate_frequently_used(frequency_data)
--- spec/services/calculate_data_by_lens_spec.rb
+++ spec/services/calculate_data_by_lens_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe CalculateDataByLens, type: :service do
   describe '#call' do
     let(:lens) { create(:lens) }
 
-    let(:exif_1) { create(:exif, lensRef: lens.id) }
-    let(:exif_2) { create(:exif, lensRef: lens.id) }
-    let(:exif_3) { create(:exif, lensRef: lens.id) }
+    let(:camera_1) { create(:camera, value: 'Canon EOS 6D') }
+    let(:camera_2) { create(:camera, value: 'Canon EOS 5D Mark IV') }
+
+    let(:exif_1) { create(:exif, lensRef: lens.id, cameraModelRef: camera_1.id) }
+    let(:exif_2) { create(:exif, lensRef: lens.id, cameraModelRef: camera_2.id) }
+    let(:exif_3) { create(:exif, lensRef: lens.id, cameraModelRef: camera_2.id) }
 
     before do
       exif_1
@@ -47,6 +50,84 @@ RSpec.describe CalculateDataByLens, type: :service do
 
       it 'lists the most frequently used keywords for that lens' do
         expect(service.call[:keywords]).to eq ['kitten', 'cat', 'portrait']
+      end
+    end
+
+    context 'cameras' do
+      it 'lists the most frequently used camera models for that lens' do
+        expect(service.call[:cameras]).to eq [camera_2.value, camera_1.value]
+      end
+    end
+
+    context 'focal_lengths' do
+      before do
+        exif_1.update!(focalLength: 35.0)
+        exif_2.update!(focalLength: 100.0)
+        exif_3.update!(focalLength: 35.0)
+      end
+
+      it 'lists the most frequently used focal lengths for that lens' do
+        expect(service.call[:focal_lengths]).to eq [35, 100]
+      end
+    end
+
+    context 'shutter_speeds' do
+      before do
+        exif_1.update!(shutterSpeed: 5.91)
+        exif_2.update!(shutterSpeed: 10.643856)
+        exif_3.update!(shutterSpeed: 5.91)
+      end
+
+      it 'lists the most frequently used shutter speeds for that lens' do
+        expect(service.call[:shutter_speeds]).to eq [60, 1600]
+      end
+    end
+
+    context 'isos' do
+      before do
+        exif_1.update!(isoSpeedRating: 1600.0)
+        exif_2.update!(isoSpeedRating: 100.0)
+        exif_3.update!(isoSpeedRating: 100.0)
+      end
+
+      it 'lists the most frequently used isos for that lens' do
+        expect(service.call[:isos]).to eq [100, 1600]
+      end
+    end
+
+    context 'months' do
+      before do
+        exif_1.update!(dateMonth: 5.0)
+        exif_2.update!(dateMonth: 8.0)
+        exif_3.update!(dateMonth: 8.0)
+      end
+
+      it 'lists the most frequent months for that lens' do
+        expect(service.call[:months]).to eq ['August', 'May']
+      end
+    end
+
+    context 'years' do
+      before do
+        exif_1.update!(dateYear: 2018.0)
+        exif_2.update!(dateYear: 2018.0)
+        exif_3.update!(dateYear: 2017.0)
+      end
+
+      it 'lists the most frequent years for that lens' do
+        expect(service.call[:years]).to eq [2018, 2017]
+      end
+    end
+
+    context 'month year combinations' do
+      before do
+        exif_1.update!(dateMonth: 5.0, dateYear: 2017.0)
+        exif_2.update!(dateMonth: 6.0, dateYear: 2018.0)
+        exif_3.update!(dateMonth: 5.0, dateYear: 2017.0)
+      end
+
+      it 'lists the most frequent month year combinations for that lens' do
+        expect(service.call[:month_year_combinations]).to eq ['May 2017', 'June 2018']
       end
     end
   end
